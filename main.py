bbd23debea691f7e38b0fd8e2962d0c3534f6bef
tgnelson19 <nelson.tyler.g@gmail.com>
2023-09-04 20:41:22 -0500

    Formatting Changes

--- main.py
+++ main.py
@@ -3,20 +3,16 @@ from button import Buttons
 from variables import Variables
 from background import Background
 
-###
-# Window Initializers
-###
 pygame.init() #Initializes a window
 
 vars = Variables() #Variable Holster Object
+
 screen = pygame.display.set_mode([vars.sW, vars.sH]) #Makes a screen that's that wide
 
-background = Background()
+background = Background() #Background holster object
 
 while not vars.done: #Determines what is rendered each frame
 
     vars.eventHandler()
     background.drawMainBackground(screen)
     vars.finishPaint(screen)
-
-    
