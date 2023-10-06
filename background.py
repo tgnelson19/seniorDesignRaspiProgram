2e59efe35754b6807febedb5926ee4e623670bb1
arambo <ramireza1265@gmail.com>
2023-10-06 10:21:48 -0500

    Alan's First Commit

--- background.py
+++ background.py
@@ -6,6 +6,8 @@ class Background:
         self.frontUIBackground = pygame.Rect(20, 20, 760, 440)  # Foreground Background Color Block
         self.leftUIFrame = pygame.Rect(40, 40, 500, 400)  # The left UI frame
         self.rightUIFrame = pygame.Rect(560, 40, 200, 400)  # The right UI frame
+        
+        #Alan's First Comment
 
     def drawMainBackground(self, screen):
         pygame.draw.rect(screen, (40, 40, 40), self.bigBackBackground)
