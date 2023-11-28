516ff65fb94515a2d5b4d215242285a5366973dd
tgnelson19 <nelson.tyler.g@gmail.com>
2023-11-27 18:09:15 -0600

    Fixing current object detection issue

--- background.py
+++ background.py
@@ -2,14 +2,11 @@ import pygame
 
 class Background:
     def __init__(self):
-        #self.bigBackBackground = pygame.Rect(0, 0, 800, 480)  # Background Color Block
-        self.frontUIBackground = pygame.Rect(0, 0, 800, 480)  # Foreground Background Color Block
-        self.leftUIFrame = pygame.Rect(20, 20, 520, 440)  # The left UI frame
-        self.rightUIFrame = pygame.Rect(560, 20, 220, 440)  # The right UI frame
-        
+        self.frontUIBackground = pygame.Rect(0, 0, 800, 480)  #Backgroung rectangle 
+        self.leftUIFrame = pygame.Rect(20, 20, 520, 440)  #The left UI frame placement
+        self.rightUIFrame = pygame.Rect(560, 20, 220, 440)  #The right UI frame
 
     def drawMainBackground(self, screen):
-       # pygame.draw.rect(screen, (40, 40, 40), self.bigBackBackground)
-        pygame.draw.rect(screen, (200, 200, 50), self.frontUIBackground)
-        pygame.draw.rect(screen, (200, 200, 200), self.leftUIFrame)
-        pygame.draw.rect(screen, (200, 200, 200), self.rightUIFrame)
+        pygame.draw.rect(screen, (200, 200, 50), self.frontUIBackground) #Paints background with given RGB
+        pygame.draw.rect(screen, (200, 200, 200), self.leftUIFrame) #Paints left UI frame with given RGB
+        pygame.draw.rect(screen, (200, 200, 200), self.rightUIFrame) #Paints right UI frame with given RGB
